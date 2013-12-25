af3b474cfbeefd032cdc1b071619d7129010811d
Peter Abeles <peter.abeles@gmail.com>
2013-12-24 18:02:33 -0600

    adding setup for affinevgg

---
+++ data/affinevgg/setup.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+mkdir bark;tar -xzf bark.tar.gz -C bark
+mkdir bikes;tar -xzf bikes.tar.gz -C bikes
+mkdir boat;tar -xzf boat.tar.gz -C boat
+mkdir graf;tar -xzf graf.tar.gz -C graf
+mkdir leuven;tar -xzf leuven.tar.gz -C leuven
+mkdir trees;tar -xzf trees.tar.gz -C trees
+mkdir ubc;tar -xzf ubc.tar.gz -C ubc
+mkdir wall;tar -xzf wall.tar.gz -C wall
