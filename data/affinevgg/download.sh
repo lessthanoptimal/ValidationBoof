94d532168ed6f99d3cc3f3ca48c607dd473d232e
Peter Abeles <peter.abeles@gmail.com>
2016-08-05 16:51:33 -0700

    - updated data source instructions

---
+++ data/affinevgg/download.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget http://www.robots.ox.ac.uk/~vgg/research/affine/det_eval_files/bikes.tar.gz
+wget http://www.robots.ox.ac.uk/~vgg/research/affine/det_eval_files/trees.tar.gz
+wget http://www.robots.ox.ac.uk/~vgg/research/affine/det_eval_files/graf.tar.gz
+wget http://www.robots.ox.ac.uk/~vgg/research/affine/det_eval_files/wall.tar.gz
+wget http://www.robots.ox.ac.uk/~vgg/research/affine/det_eval_files/bark.tar.gz
+wget http://www.robots.ox.ac.uk/~vgg/research/affine/det_eval_files/boat.tar.gz
+wget http://www.robots.ox.ac.uk/~vgg/research/affine/det_eval_files/leuven.tar.gz
+wget http://www.robots.ox.ac.uk/~vgg/research/affine/det_eval_files/ubc.tar.gz
