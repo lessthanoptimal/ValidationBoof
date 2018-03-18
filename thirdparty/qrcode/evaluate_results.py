94134be9b245ad0f1253327797e9bb7180743093
Peter Abeles <peter.abeles@gmail.com>
2018-03-18 11:16:06 -0700

    - can plot results now

--- thirdparty/qrcode/evaluate_results.py
+++ thirdparty/qrcode/evaluate_results.py
@@ -102,10 +102,20 @@ def compare_results( expected , found ):
 
     return {"tp":true_positive,"fp":false_positive,"fn":false_negative,"ambiguous":ambiguous}
 
+
+def compute_f( tp , fp , tn , fn ):
+    return 2.0*tp/(2.0*tp + fp + fn)
+
 options, arguments = p.parse_args()
 
 dir_images = options.Images
 dir_results = options.Results
+
+# Score information for each library is stored here
+library_scores = {}
+
+# Number of images in each category is stored here
+category_counts = {}
 
 # List of all the datasets
 data_sets = [d for d in os.listdir(dir_images) if os.path.isdir(join(dir_images,d))]
@@ -130,6 +140,8 @@ for target_name in os.listdir(dir_results):
         path_ds_truth = join(dir_images,ds)
 
         truth_files = [f for f in os.listdir(path_ds_truth) if f.endswith("txt")]
+
+        category_counts[ds] = len(truth_files)
 
         ds_true_positive = 0
         ds_false_positive = 0
@@ -167,7 +179,72 @@ for target_name in os.listdir(dir_results):
     print("  true positive    {}".format(total_true_positive))
     print("  ambiguous        {}".format(total_ambiguous))
 
+    scoresF = {}
+
+    scoresF["summary"] = compute_f(total_true_positive,total_false_positive,0,total_false_negative)
     for n in ds_results:
         r = ds_results[n]
-        F = 2*r['tp']/(2*r['tp'] + r['fp'] + r['fn'])
+        F = compute_f(r['tp'],r['fp'],0,r['fn'])
+        scoresF[n] = F
         print("{:15s} F={:.2f}".format(n,F))
+
+    library_scores[target_name] = scoresF
+
+print()
+
+
+# Create the plot showing a summary by category
+import matplotlib.pyplot as plt
+import numpy as np
+
+library_names = sorted(list(library_scores.keys()))
+categories = sorted(list(library_scores[library_names[0]].keys()))
+categories = [x for x in categories if x is not "summary"]
+count_list = [category_counts[x] for x in categories]
+
+indexes = np.arange(len(categories))
+fig, ax = plt.subplots()
+
+bar_width = 0.35
+bar_space = 1.5
+
+for idx,lib_name in enumerate(library_names):
+    scores_map = library_scores[lib_name]
+    scores_list = [scores_map[x] for x in categories]
+    ax.bar(indexes*bar_space+idx*bar_width,scores_list,bar_width,label=lib_name)
+
+ax.set_ylim([0.0,1.0])
+ax.set_ylabel("F-Metric")
+ax.set_title('Detection Performance by Categories')
+ax.set_xticks(indexes*bar_space + (bar_width*int(len(library_names)/2)) )
+ax.set_xticklabels(categories, rotation=90)
+ax.legend()
+
+plt.gcf().subplots_adjust(bottom=0.25)
+plt.savefig("performance_categories.pdf", format='pdf')
+plt.close()
+
+# Image Count by Category
+fig, ax = plt.subplots()
+indexes = np.arange(len(categories))
+ax.bar(indexes,count_list)
+ax.set_ylabel("Number of Images")
+ax.set_title('Total Images by Categories')
+ax.set_xticks(indexes)
+ax.set_xticklabels(categories, rotation=90)
+plt.gcf().subplots_adjust(bottom=0.25)
+plt.savefig("category_counts.pdf", format='pdf')
+plt.close()
+
+# Create plot Showing overall summary
+fig, ax = plt.subplots()
+indexes = np.arange(len(library_names))
+library_summary = [library_scores[x]['summary'] for x in library_names]
+ax.bar(indexes,library_summary)
+ax.set_ylim([0.0,1.0])
+ax.set_ylabel("F-Metric")
+ax.set_title('Overall Detection Performance')
+ax.set_xticks(indexes)
+ax.set_xticklabels(library_names, rotation=90)
+plt.gcf().subplots_adjust(bottom=0.15)
+plt.savefig("performance_summary.pdf", format='pdf')
