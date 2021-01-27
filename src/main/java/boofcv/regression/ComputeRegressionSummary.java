bd02aebde49548a9f433bf9d7cc499b8bfe20508
Peter Abeles <peter.abeles@gmail.com>
2021-01-26 18:41:39 -0800

    - Added machine name to e-mail summary

--- src/main/java/boofcv/regression/ComputeRegressionSummary.java
+++ src/main/java/boofcv/regression/ComputeRegressionSummary.java
@@ -3,6 +3,7 @@ package boofcv.regression;
 import boofcv.BoofVersion;
 import boofcv.ValidationBoofVersion;
 import boofcv.common.BoofRegressionConstants;
+import boofcv.common.SettingsLocal;
 import georegression.GeoRegressionVersion;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.FileFilterUtils;
@@ -48,12 +49,12 @@ public class ComputeRegressionSummary {
      *
      * @param file File with e-mail information
      */
-    public void emailSummary( File file ) throws IOException {
-        if( !file.exists() ) {
+    public void emailSummary(File file) throws IOException {
+        if (!file.exists()) {
             System.err.println("E-Mail login file doesn't exist");
             return;
         }
-        if( summary == null )
+        if (summary == null)
             throw new RuntimeException("You must compute the summary before calling this function");
 
         BufferedReader reader = new BufferedReader(new FileReader(file));
@@ -66,17 +67,17 @@ public class ComputeRegressionSummary {
     }
 
     public void generateSummary() throws IOException {
-        computeMD5(new File(BoofRegressionConstants.BASELINE_DIRECTORY),baselineMD5);
-        computeMD5(new File(BoofRegressionConstants.CURRENT_DIRECTORY),currentMD5);
+        computeMD5(new File(BoofRegressionConstants.BASELINE_DIRECTORY), baselineMD5);
+        computeMD5(new File(BoofRegressionConstants.CURRENT_DIRECTORY), currentMD5);
 
-        for( String key : currentMD5.keySet() ) {
+        for (String key : currentMD5.keySet()) {
             byte[] c = currentMD5.get(key);
             byte[] b = baselineMD5.get(key);
 
-            if( b == null ) {
+            if (b == null) {
                 currentUnique.add(key);
             } else {
-                if( !Arrays.equals(c,b) ) {
+                if (!Arrays.equals(c, b)) {
                     missmatched.add(key);
                 } else {
                     totalMatched++;
@@ -84,8 +85,8 @@ public class ComputeRegressionSummary {
             }
         }
 
-        for( String key : baselineMD5.keySet() ) {
-            if( !currentMD5.containsKey(key)) {
+        for (String key : baselineMD5.keySet()) {
+            if (!currentMD5.containsKey(key)) {
                 baselineUnique.add(key);
             }
         }
@@ -99,10 +100,10 @@ public class ComputeRegressionSummary {
 
         summary = createSummary();
 
-        FileUtils.write(new File(BoofRegressionConstants.CURRENT_DIRECTORY,"summary.txt"),summary,"UTF-8");
+        FileUtils.write(new File(BoofRegressionConstants.CURRENT_DIRECTORY, "summary.txt"), summary, "UTF-8");
     }
 
-    private void sendEmailSSL(String summary ) {
+    private void sendEmailSSL(String summary) {
         Properties props = new Properties();
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.socketFactory.port", "465");
@@ -114,22 +115,22 @@ public class ComputeRegressionSummary {
         Session session = Session.getDefaultInstance(props,
                 new javax.mail.Authenticator() {
                     protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(emailUsername,emailPassword);
+                        return new PasswordAuthentication(emailUsername, emailPassword);
                     }
                 });
 
         try {
-            String noticed = "  "+(hasMissMatch ? "Miss Match" : "");
+            String noticed = "  " + (hasMissMatch ? "Miss Match" : "");
 
             Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(emailUsername+"@gmail.com"));
-            message.setRecipients(Message.RecipientType.TO,InternetAddress.parse(emailDestination));
-            message.setSubject("ValidationBoof Summary"+noticed);
+            message.setFrom(new InternetAddress(emailUsername + "@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(emailDestination));
+            message.setSubject("ValidationBoof Summary" + noticed);
             message.setContent(summary, "text/html; charset=utf-8");
 
             Transport.send(message);
 
-            System.out.println("Sent summary to "+emailDestination);
+            System.out.println("Sent summary to " + emailDestination);
 
         } catch (MessagingException e) {
             throw new RuntimeException(e);
@@ -140,30 +141,31 @@ public class ComputeRegressionSummary {
         String summary = "";
 
         summary += "<!DOCTYPE html><html lang=\"en\"><body><pre>\n";
-        summary += "Finished Time:         "+MasterRegressionApplication.formatDate(new Date())+"\n";
-        summary += "Elapsed Time:          "+MasterRegressionApplication.formatElapsed(elapsedTime)+"\n";
+        summary += "Finished Time:         " + MasterRegressionApplication.formatDate(new Date()) + "\n";
+        summary += "Elapsed Time:          " + MasterRegressionApplication.formatElapsed(elapsedTime) + "\n";
         summary += "\n";
 
-        summary += String.format("%18s %15s %20.20s %5s\n","Name","Version","SHA","Dirty");
-        summary += String.format("%18s %15s %20.20s %5s\n","BoofCV",
-                BoofVersion.VERSION, BoofVersion.GIT_SHA,(0!=BoofVersion.DIRTY));
-        summary += String.format("%18s %15s %20.20s %5s\n","ValidationBoof",
-                ValidationBoofVersion.VERSION, ValidationBoofVersion.GIT_SHA,(0!=ValidationBoofVersion.DIRTY));
-        summary += String.format("%18s %15s %20.20s %5s\n","GeoRegression",
-                GeoRegressionVersion.VERSION, GeoRegressionVersion.GIT_SHA,(0!=GeoRegressionVersion.DIRTY));
-        summary += String.format("%18s %15s %20.20s %5s\n","DDogleg",
-                DDoglegVersion.VERSION, DDoglegVersion.GIT_SHA,(0!=DDoglegVersion.DIRTY));
-        summary += String.format("%18s %15s %20.20s %5s\n","EJML",
-                EjmlVersion.VERSION, EjmlVersion.GIT_SHA,(0!=EjmlVersion.DIRTY));
+        summary += String.format("%18s %15s %20.20s %5s\n", "Name", "Version", "SHA", "Dirty");
+        summary += String.format("%18s %15s %20.20s %5s\n", "BoofCV",
+                BoofVersion.VERSION, BoofVersion.GIT_SHA, (0 != BoofVersion.DIRTY));
+        summary += String.format("%18s %15s %20.20s %5s\n", "ValidationBoof",
+                ValidationBoofVersion.VERSION, ValidationBoofVersion.GIT_SHA, (0 != ValidationBoofVersion.DIRTY));
+        summary += String.format("%18s %15s %20.20s %5s\n", "GeoRegression",
+                GeoRegressionVersion.VERSION, GeoRegressionVersion.GIT_SHA, (0 != GeoRegressionVersion.DIRTY));
+        summary += String.format("%18s %15s %20.20s %5s\n", "DDogleg",
+                DDoglegVersion.VERSION, DDoglegVersion.GIT_SHA, (0 != DDoglegVersion.DIRTY));
+        summary += String.format("%18s %15s %20.20s %5s\n", "EJML",
+                EjmlVersion.VERSION, EjmlVersion.GIT_SHA, (0 != EjmlVersion.DIRTY));
         summary += "\n";
-        summary += "java.runtime.version:  "+System.getProperty("java.runtime.version")+"\n";
-        summary += "java.vendor:           "+System.getProperty("java.vendor")+"\n";
-        summary += "os.name+arch:          "+System.getProperty("os.name")+" "+System.getProperty("os.arch")+"\n";
-        summary += "os.version:            "+System.getProperty("os.version")+"\n";
+        summary += "machine:               " + SettingsLocal.machineName + "\n";
+        summary += "java.runtime.version:  " + System.getProperty("java.runtime.version") + "\n";
+        summary += "java.vendor:           " + System.getProperty("java.vendor") + "\n";
+        summary += "os.name+arch:          " + System.getProperty("os.name") + " " + System.getProperty("os.arch") + "\n";
+        summary += "os.version:            " + System.getProperty("os.version") + "\n";
         summary += "\n";
-        summary += "Total Matched:         "+totalMatched+"\n\n";
+        summary += "Total Matched:         " + totalMatched + "\n\n";
 
-        if( missmatched.isEmpty() && baselineUnique.isEmpty() && currentUnique.isEmpty() ) {
+        if (missmatched.isEmpty() && baselineUnique.isEmpty() && currentUnique.isEmpty()) {
             summary += "NO EXCEPTIONS!\n";
         } else {
             summary += "Total Miss Matched:    " + missmatched.size() + "\n";
@@ -171,7 +173,7 @@ public class ComputeRegressionSummary {
             summary += "Total Current Unique:  " + currentUnique.size() + "\n";
             summary += "\n";
 
-            if (missmatched.size()>0) {
+            if (missmatched.size() > 0) {
                 summary += "Miss Matched\n";
                 for (String s : missmatched) {
                     summary += "  " + s + "\n";
@@ -179,14 +181,14 @@ public class ComputeRegressionSummary {
                 summary += "\n";
             }
 
-            if (baselineUnique.size()>0) {
+            if (baselineUnique.size() > 0) {
                 summary += "Baseline Unique\n";
                 for (String s : baselineUnique) {
                     summary += "  " + s + "\n";
                 }
                 summary += "\n";
             }
-            if (currentUnique.size()>0) {
+            if (currentUnique.size() > 0) {
                 summary += "Current Unique\n";
                 for (String s : currentUnique) {
                     summary += "  " + s + "\n";
@@ -205,7 +207,7 @@ public class ComputeRegressionSummary {
     }
 
     private void computeMD5(File directory, Map<String, byte[]> output) {
-        Collection<File> files = FileUtils.listFiles(directory, FileFilterUtils.prefixFileFilter("ACC_"),FileFilterUtils.directoryFileFilter());
+        Collection<File> files = FileUtils.listFiles(directory, FileFilterUtils.prefixFileFilter("ACC_"), FileFilterUtils.directoryFileFilter());
 
         try {
             MessageDigest md = MessageDigest.getInstance("MD5");
@@ -223,17 +225,17 @@ public class ComputeRegressionSummary {
                 String parent1 = f.getParentFile().getName();
 
                 File path;
-                if( parent1.toLowerCase().equals("other")) {
+                if (parent1.toLowerCase().equals("other")) {
                     path = new File(parent1);
                 } else {
-                    path = new File(parent0,parent1);
+                    path = new File(parent0, parent1);
                 }
 
-                String key = new File(path,f.getName()).toString();
+                String key = new File(path, f.getName()).toString();
 
-                output.put(key,thedigest);
+                output.put(key, thedigest);
             }
-        } catch( IOException | NoSuchAlgorithmException e ) {
+        } catch (IOException | NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
     }
