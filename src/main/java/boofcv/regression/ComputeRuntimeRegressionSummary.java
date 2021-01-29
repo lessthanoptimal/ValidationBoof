ec88d63382f85b4b4997d5674e4447b4ece567fe
Peter Abeles <peter.abeles@gmail.com>
2021-01-28 16:24:17 -0800

    - Fixed issue where runtime results in current but not in regression were not reported as an exception

--- src/main/java/boofcv/regression/ComputeRuntimeRegressionSummary.java
+++ src/main/java/boofcv/regression/ComputeRuntimeRegressionSummary.java
@@ -44,21 +44,21 @@ public class ComputeRuntimeRegressionSummary {
             return resultsToString();
         } catch (Exception e) {
             e.printStackTrace(err);
-            return "Exception computing runtime summary.\n"+
-                    "  Type="+e.getClass().getName()+"\n"+
-                    "  Message="+e.getMessage()+"\n";
+            return "Exception computing runtime summary.\n" +
+                    "  Type=" + e.getClass().getName() + "\n" +
+                    "  Message=" + e.getMessage() + "\n";
         }
     }
 
     private String resultsToString() {
-        String message = "Runtime Summary: Significant="+threshold+" Metric="+ParseRuntimeResults.DEFAULT_METRIC+"\n\n";
+        String message = "Runtime Summary: Significant=" + threshold + " Metric=" + ParseRuntimeResults.DEFAULT_METRIC + "\n\n";
         message += "compared    flagged   miss_match  exceptions\n";
         message += String.format("%5d       %5d      %5d      %5d\n",
-                resultsCompared,flagged.size(),missMatches.size(),exceptions.size());
+                resultsCompared, flagged.size(), missMatches.size(), exceptions.size());
         if (!flagged.isEmpty()) {
             message += String.format("\nflagged.size=%d\n", flagged.size());
             for (Flagged f : flagged) {
-                message += String.format("  %5.1f%% %s\n",100.0*(f.currentOverBaseline),f.file +":"+ f.group+":"+ f.field);
+                message += String.format("  %5.1f%% %s\n", 100.0 * (f.currentOverBaseline), f.file + ":" + f.group + ":" + f.field);
             }
         }
         if (!missMatches.isEmpty()) {
@@ -86,108 +86,140 @@ public class ComputeRuntimeRegressionSummary {
         findAllRuntimeResults(SettingsLocal.getPathToBaselineRuntimeDirectory(), baselinePaths);
         findAllRuntimeResults(SettingsLocal.getPathToCurrentRuntimeDirectory(), currentPaths);
 
-        for ( String baselinePath : baselinePaths ) {
-            String shortBaselinePath = truncateBaselinePath(baselinePath);
-            String currentPath = findCorrespondingPath(baselinePath, currentPaths);
+        for (String baselinePath : baselinePaths) {
+            String currentPath = findCorrespondingPath(true, baselinePath, currentPaths);
+            String shortBaselinePath = truncateBaselinePath(true, baselinePath);
 
             // Check to see if the current has these results from the baseline
-            if (currentPath==null) {
-                missMatches.add("No matching base: "+shortBaselinePath);
+            if (currentPath == null) {
+                missMatches.add("File not in current: " + shortBaselinePath);
                 continue;
             }
 
-            // Try parsing the results and if there are any exceptions log them
-            try {
-                rawBaseline.parse(new FileInputStream(baselinePath));
-            } catch (IOException e) {
-                exceptions.add(e.getMessage()+" "+shortBaselinePath);
+            // Loads results for the specific metric from current and baseline
+            if (loadResults(baselinePath, shortBaselinePath, currentPath))
                 continue;
-            }
-            try {
-                rawCurrent.parse(new FileInputStream(currentPath));
-            } catch (IOException e) {
-                exceptions.add(e.getMessage()+" "+shortBaselinePath);
-                continue;
-            }
 
             // See if the file override this metric
             String targetMetric = rawBaseline.targetMetric;
 
             // indicates that the target metric was found at least once
             boolean foundTargetMetric = false;
-            for( ParseRuntimeResults.Group groupBaseline : rawBaseline.groups ) {
+            for (ParseRuntimeResults.Group groupBaseline : rawBaseline.groups) {
                 ParseRuntimeResults.Group groupCurrent = rawCurrent.findGroup(groupBaseline.name);
-                if (groupCurrent==null) {
-                    exceptions.add("No corresponding group: "+groupBaseline.name+" in "+shortBaselinePath);
+                if (groupCurrent == null) {
+                    exceptions.add("No corresponding group: " + groupBaseline.name + " in " + shortBaselinePath);
                     continue;
                 }
 
                 if (groupBaseline.results.size() != groupCurrent.results.size())
-                    exceptions.add("Result size miss match: "+shortBaselinePath+" "+groupBaseline.name);
+                    exceptions.add("Result size miss match: " + shortBaselinePath + " " + groupBaseline.name);
 
                 // find the ID of the metric being evaluated
                 int targetBaseline = groupBaseline.indexOfMetric(targetMetric);
                 int targetCurrent = groupCurrent.indexOfMetric(targetMetric);
 
                 // The targeted metric is not in this group so skip it
-                if (targetBaseline==-1)
+                if (targetBaseline == -1)
                     continue;
                 foundTargetMetric = true;
 
-                int unmatched = 0;
-                for (int idxResults = 0; idxResults < groupBaseline.results.size(); idxResults++) {
-                    ParseRuntimeResults.Result resultBaseline = groupBaseline.results.get(idxResults);
-                    ParseRuntimeResults.Result resultCurrent = groupCurrent.lookup(resultBaseline.name);
-                    if (resultCurrent==null) {
-                        unmatched++;
-                        continue;
-                    }
-
-                    // Look up the found metric values
-                    double b = resultBaseline.results.get(targetBaseline);
-                    double c = resultCurrent.results.get(targetCurrent);
-
-                    if (b==0.0) {
-                        exceptions.add("Base took zero seconds: "+shortBaselinePath+" "+groupBaseline.name+" "+resultBaseline.name);
-                        continue;
-                    }
-                    resultsCompared++;
-
-                    if (threshold<(c>b?c/b:b/c)-1.0) {
-                        flagged.add(new Flagged(shortBaselinePath,groupBaseline.name,resultBaseline.name,c/b));
-                    }
-                }
-
-                // if the result count doesn't match this error has already been reported
-                if (unmatched==0 || (groupBaseline.results.size() != groupCurrent.results.size()))
-                    continue;
-
-                missMatches.add("Unmatched results: "+shortBaselinePath+" "+groupBaseline.name+
-                        " "+unmatched+"/"+groupBaseline.results.size());
+                compareMetricsForBenchmark(shortBaselinePath, groupBaseline, groupCurrent, targetBaseline, targetCurrent);
             }
 
             // Print an error if it couldn't perform any runtime regression on this file
             if (!foundTargetMetric) {
-                exceptions.add("Metric not found. "+shortBaselinePath);
+                exceptions.add("Metric not found. " + shortBaselinePath);
             }
         }
+
+        findResultsNotInBaseline(baselinePaths, currentPaths);
+    }
+
+    private void compareMetricsForBenchmark(String shortBaselinePath,
+                                            ParseRuntimeResults.Group groupBaseline,
+                                            ParseRuntimeResults.Group groupCurrent,
+                                            int targetBaseline, int targetCurrent) {
+        int unmatched = 0;
+        for (int idxResults = 0; idxResults < groupBaseline.results.size(); idxResults++) {
+            ParseRuntimeResults.Result resultBaseline = groupBaseline.results.get(idxResults);
+            ParseRuntimeResults.Result resultCurrent = groupCurrent.lookup(resultBaseline.name);
+            if (resultCurrent == null) {
+                unmatched++;
+                continue;
+            }
+
+            // Look up the found metric values
+            double b = resultBaseline.results.get(targetBaseline);
+            double c = resultCurrent.results.get(targetCurrent);
+
+            if (b == 0.0) {
+                exceptions.add("Base took zero seconds: " + shortBaselinePath + " " + groupBaseline.name + " " + resultBaseline.name);
+                continue;
+            }
+            resultsCompared++;
+
+            if (threshold < (c > b ? c / b : b / c) - 1.0) {
+                flagged.add(new Flagged(shortBaselinePath, groupBaseline.name, resultBaseline.name, c / b));
+            }
+        }
+
+        // if the result count doesn't match this error has already been reported
+        if (unmatched == 0 || (groupBaseline.results.size() != groupCurrent.results.size()))
+            return;
+
+        missMatches.add("Unmatched results: " + shortBaselinePath + " " + groupBaseline.name +
+                " " + unmatched + "/" + groupBaseline.results.size());
+    }
+
+    private void findResultsNotInBaseline(List<String> baselinePaths, List<String> currentPaths) {
+        for (String currentPath : currentPaths) {
+            String baselinePath = findCorrespondingPath(false, currentPath, baselinePaths);
+
+            if (baselinePath != null)
+                continue;
+
+            String shortCurrentPath = truncateBaselinePath(false, currentPath);
+            missMatches.add("File not in baseline: " + shortCurrentPath);
+        }
+    }
+
+    private boolean loadResults(String baselinePath, String shortBaselinePath, String currentPath) {
+        // Try parsing the results and if there are any exceptions log them
+        try {
+            rawBaseline.parse(new FileInputStream(baselinePath));
+        } catch (IOException e) {
+            exceptions.add(e.getMessage() + " " + shortBaselinePath);
+            return true;
+        }
+        try {
+            rawCurrent.parse(new FileInputStream(currentPath));
+        } catch (IOException e) {
+            exceptions.add(e.getMessage() + " " + shortBaselinePath);
+            return true;
+        }
+        return false;
     }
 
     /**
      * Shorten the path to make it more readable
      */
-    private String truncateBaselinePath( String path ) {
-        String baselinePref = SettingsLocal.getPathToBaselineRuntimeDirectory().getPath();
-        return path.substring(baselinePref.length()+1);
+    private String truncateBaselinePath(boolean baseline, String path) {
+        String baselinePref = baseline ?
+                SettingsLocal.getPathToBaselineRuntimeDirectory().getPath() :
+                SettingsLocal.getPathToCurrentRuntimeDirectory().getPath();
+        return path.substring(baselinePref.length() + 1);
     }
 
     /**
      * Finds the corresponding directory in current results to the results in baseline
      */
-    private String findCorrespondingPath(String baselinePath, List<String> currentPaths) {
-        String baselinePref = SettingsLocal.getPathToBaselineRuntimeDirectory().getPath();
+    private String findCorrespondingPath(boolean baseline, String baselinePath, List<String> currentPaths) {
+        String baselinePref = baseline ?
+                SettingsLocal.getPathToBaselineRuntimeDirectory().getPath() :
+                SettingsLocal.getPathToCurrentRuntimeDirectory().getPath();
 
-        String ending = baselinePath.substring(baselinePref.length());
+        String ending = baselinePath.substring(baselinePref.length() + 1);
 
         for (int i = 0; i < currentPaths.size(); i++) {
             if (currentPaths.get(i).endsWith(ending))
@@ -200,9 +232,9 @@ public class ComputeRuntimeRegressionSummary {
     /**
      * Recursively searches for all runtime results in the path
      */
-    public void findAllRuntimeResults( File root, List<String> found ) {
+    public void findAllRuntimeResults(File root, List<String> found) {
         File[] files = root.listFiles();
-        if (files==null)
+        if (files == null)
             return;
 
         // Find all the runtime files
