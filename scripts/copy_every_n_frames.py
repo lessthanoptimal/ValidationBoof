27cfb260bb1dc34f81b34acfc2f18600be476002
Peter Abeles <peter.abeles@gmail.com>
2021-09-03 21:29:42 -0700

    - Updating results - Updated script to Python 3

--- scripts/copy_every_n_frames.py
+++ scripts/copy_every_n_frames.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import optparse
 import os
@@ -12,7 +12,6 @@ p.add_option('--output', '-o', default="output")
 p.add_option('--skip', '-s', default=10)
 p.add_option('--keepname', '-k', action="store_false")
 
-
 options, arguments = p.parse_args()
 
 dir_input = options.input
@@ -23,8 +22,8 @@ if not dir_input:
     p.print_help()
     exit(0)
 
-print "Skip        {}".format(options.skip)
-print "Rename      {}".format(not options.keepname)
+print("Skip        {}".format(options.skip))
+print("Rename      {}".format(not options.keepname))
 
 # Create output directory if it doesn't exist
 if not os.path.exists(dir_output):
@@ -33,7 +32,7 @@ if not os.path.exists(dir_output):
 file_list = [f for f in listdir(dir_input) if isfile(join(dir_input, f))]
 file_list.sort()
 
-print "Total files in {}".format(len(file_list))
+print("Total files in {}".format(len(file_list)))
 
 count = 0
 for f in file_list:
@@ -44,6 +43,6 @@ for f in file_list:
     dst = f
     suffix = f.split(".")[1]
     if not options.keepname:
-        dst = "image{:04d}.{:s}".format(count/skip,suffix)
+        dst = "image{:04d}.{:s}".format(round(count/skip),suffix)
     copyfile(join(dir_input,f), join(dir_output,dst))
 
