7c95c4cc0b4a546c629914c47f86b5468e8ae8fa
Peter Abeles <peter.abeles@gmail.com>
2021-09-05 09:15:23 -0700

    - Added hamming regression to download script

--- data/fiducials/download.py
+++ data/fiducials/download.py
@@ -5,7 +5,7 @@ from subprocess import call
 
 files = ["chessboard_v2.zip","circle_hexagonal_v1_1.zip","circle_regular_v1.zip",
          "square_border_binary_v1.zip","square_border_image_v1.zip","square_grid_v1.zip","qrcodes_v3.zip",
-         "random_dots_v1.zip"]
+         "random_dots_v1.zip", "square_border_hamming_v1.zip"]
 
 for f in files:
     print("retrieving "+f)
