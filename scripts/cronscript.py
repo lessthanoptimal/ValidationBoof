f2efdc5c2631dd4ce7086d20c142bbaf87dd2464
Peter Abeles <peter.abeles@gmail.com>
2021-09-28 11:10:41 -0700

    Fixed cronscript

--- scripts/cronscript.py
+++ scripts/cronscript.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
-# Run the other scripts
+import os
+
+if os.system("python3 regression_performance.py"):
+    print("Failed to run jmh")
+
+if os.system("python3 regression_jmh.py"):
+    print("Failed to run jmh")
 
 print("Master Cronscript Done!")
