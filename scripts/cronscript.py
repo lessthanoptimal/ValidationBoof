85f88bd3bc4d447d3d5018665bb490ed16e701ae
Peter Abeles <peter.abeles@gmail.com>
2021-10-07 10:13:01 -0700

    fixed last commit

--- scripts/cronscript.py
+++ scripts/cronscript.py
@@ -3,7 +3,7 @@
 import os
 
 # Change directory to make it easy to find the scripts
-os.chdir(os.path.realpath(__file__))
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 if os.system("python3 regression_performance.py"):
     print("Failed to run jmh")
