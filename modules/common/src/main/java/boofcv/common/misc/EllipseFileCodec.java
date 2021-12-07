2dba85e0c1f8dbcd98530bb4b8642c92b04200a9
Peter Abeles <peter.abeles@gmail.com>
2021-12-06 16:34:46 -0800

    code formatting

--- modules/common/src/main/java/boofcv/common/misc/EllipseFileCodec.java
+++ modules/common/src/main/java/boofcv/common/misc/EllipseFileCodec.java
@@ -13,60 +13,56 @@ import static boofcv.common.misc.ParseHelper.skipComments;
  * @author Peter Abeles
  */
 public class EllipseFileCodec {
-	public static void save( String path , String header, List<EllipseRotated_F64> ellipses ) {
-		File f = new File(path);
+    public static void save(String path, String header, List<EllipseRotated_F64> ellipses) {
+        File f = new File(path);
 
-		try {
-			PrintStream out = new PrintStream(f);
+        try {
+            PrintStream out = new PrintStream(f);
 
-			if( header != null)
-				out.println("# "+header);
-			out.println("# center.x center.y a b phi");
-			out.println("# coordinates in pixels and angle in radians");
+            if (header != null)
+                out.println("# " + header);
+            out.println("# center.x center.y a b phi");
+            out.println("# coordinates in pixels and angle in radians");
 
-			for (int i = 0; i < ellipses.size(); i++) {
-				EllipseRotated_F64 e = ellipses.get(i);
-				out.printf("%.20f %.20f %.20f %.20f %.20f\n",e.center.x,e.center.y,e.a,e.b,e.phi);
-			}
+            for (int i = 0; i < ellipses.size(); i++) {
+                EllipseRotated_F64 e = ellipses.get(i);
+                out.printf("%.20f %.20f %.20f %.20f %.20f\n", e.center.x, e.center.y, e.a, e.b, e.phi);
+            }
 
-		} catch (FileNotFoundException e) {
-			throw new RuntimeException(e);
-		}
-	}
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	public static List<EllipseRotated_F64> load( String path ) {
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(path));
+    public static List<EllipseRotated_F64> load(String path) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(path));
 
-			String line = skipComments(reader);
+            String line = skipComments(reader);
 
-			List<EllipseRotated_F64> out = new ArrayList<EllipseRotated_F64>();
-			while( line != null ) {
+            List<EllipseRotated_F64> out = new ArrayList<EllipseRotated_F64>();
+            while (line != null) {
 
-				String words[] = line.split(" ");
-				if( words.length != 5 ) {
-					throw new RuntimeException("expected fives words");
-				}
+                String[] words = line.split(" ");
+                if (words.length != 5) {
+                    throw new RuntimeException("expected fives words");
+                }
 
-				EllipseRotated_F64 p = new EllipseRotated_F64();
-				p.center.x = Double.parseDouble(words[0]);
-				p.center.y = Double.parseDouble(words[1]);
-				p.a = Double.parseDouble(words[2]);
-				p.b = Double.parseDouble(words[3]);
-				p.phi = Double.parseDouble(words[4]);
+                EllipseRotated_F64 p = new EllipseRotated_F64();
+                p.center.x = Double.parseDouble(words[0]);
+                p.center.y = Double.parseDouble(words[1]);
+                p.a = Double.parseDouble(words[2]);
+                p.b = Double.parseDouble(words[3]);
+                p.phi = Double.parseDouble(words[4]);
 
-				out.add(p);
-				line = reader.readLine();
-			}
+                out.add(p);
+                line = reader.readLine();
+            }
 
-			return out;
+            return out;
 
-		} catch (FileNotFoundException e) {
-			throw new RuntimeException(e);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- modules/common/src/main/java/boofcv/common/misc/PointFileCodec.java
+++ modules/common/src/main/java/boofcv/common/misc/PointFileCodec.java
@@ -14,142 +14,138 @@ import static boofcv.common.misc.ParseHelper.skipComments;
  * @author Peter Abeles
  */
 public class PointFileCodec {
+    public static void save(String path, String header, List<Point2D_F64> points) {
+        File f = new File(path);
 
-	public static void save( String path , String header, List<Point2D_F64> points ) {
-		File f = new File(path);
+        try {
+            PrintStream out = new PrintStream(f);
 
-		try {
-			PrintStream out = new PrintStream(f);
+            if (header != null)
+                out.println("# " + header);
 
-			if( header != null)
-				out.println("# "+header);
+            for (int i = 0; i < points.size(); i++) {
+                Point2D_F64 p = points.get(i);
+                out.printf("%.20f %.20f\n", p.x, p.y);
+            }
 
-			for (int i = 0; i < points.size(); i++) {
-				Point2D_F64 p = points.get(i);
-				out.printf("%.20f %.20f\n",p.x,p.y);
-			}
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-		} catch (FileNotFoundException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    public static void saveSets(String path, String header, List<List<Point2D_F64>> points) {
+        File f = new File(path);
 
-	public static void saveSets( String path , String header, List<List<Point2D_F64>> points ) {
-		File f = new File(path);
+        try {
+            PrintStream out = new PrintStream(f);
 
-		try {
-			PrintStream out = new PrintStream(f);
+            if (header != null)
+                out.println("# " + header);
 
-			if( header != null)
-				out.println("# "+header);
+            out.println("SETS");
+            for (List<Point2D_F64> set : points) {
+                if (set.isEmpty())
+                    continue;
+                for (int i = 0; i < set.size(); i++) {
+                    Point2D_F64 p = set.get(i);
+                    out.printf("%.20f %.20f", p.x, p.y);
+                    if (i != set.size() - 1) {
+                        out.print(" ");
+                    }
+                }
+                out.println();
+            }
 
-			out.println("SETS");
-			for( List<Point2D_F64> set : points ) {
-				if( set.isEmpty() )
-					continue;
-				for (int i = 0; i < set.size(); i++) {
-					Point2D_F64 p = set.get(i);
-					out.printf("%.20f %.20f", p.x, p.y);
-					if( i != set.size()-1) {
-						out.print(" ");
-					}
-				}
-				out.println();
-			}
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-		} catch (FileNotFoundException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    public static boolean isPointSet(File path) {
+        try {
+            FileReader reader = new FileReader(path);
+            BufferedReader input = new BufferedReader(reader);
+            String line = skipComments(input);
+            reader.close();
+            return line.compareTo("SETS") == 0;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	public static boolean isPointSet( File path ) {
-		try {
-			FileReader reader = new FileReader(path);
-			BufferedReader input = new BufferedReader(reader);
-			String line = skipComments(input);
-			reader.close();
-			return line.compareTo("SETS") == 0;
-		} catch( Exception e ) {
-			throw new RuntimeException(e);
-		}
-	}
+    public static List<Point2D_F64> load(File path) {
+        return load(path.getAbsolutePath());
+    }
 
-	public static List<Point2D_F64> load( File path ) {
-		return load(path.getAbsolutePath());
-	}
-	public static List<Point2D_F64> load( String path ) {
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(path));
+    public static List<Point2D_F64> load(String path) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(path));
 
-			String line = skipComments(reader);
-			if( line.compareTo("SETS") == 0 ) {
-				System.err.println("This is a point sets file!");
-				return null;
-			}
+            String line = skipComments(reader);
+            if (line.compareTo("SETS") == 0) {
+                System.err.println("This is a point sets file!");
+                return null;
+            }
 
-			List<Point2D_F64> out = new ArrayList<Point2D_F64>();
-			while( line != null ) {
+            List<Point2D_F64> out = new ArrayList<>();
+            while (line != null) {
 
-				String words[] = line.split(" ");
-				if( words.length != 2 ) {
-					throw new RuntimeException("expected two words");
-				}
+                String[] words = line.split(" ");
+                if (words.length != 2) {
+                    throw new RuntimeException("expected two words");
+                }
 
-				Point2D_F64 p = new Point2D_F64();
-				p.x = Double.parseDouble(words[0]);
-				p.y = Double.parseDouble(words[1]);
+                Point2D_F64 p = new Point2D_F64();
+                p.x = Double.parseDouble(words[0]);
+                p.y = Double.parseDouble(words[1]);
 
-				out.add(p);
-				line = reader.readLine();
-			}
+                out.add(p);
+                line = reader.readLine();
+            }
 
-			return out;
+            return out;
 
-		} catch (FileNotFoundException e) {
-			throw new RuntimeException(e);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	public static List<List<Point2D_F64>> loadSets( File path ) {
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader(path));
+    public static List<List<Point2D_F64>> loadSets(File path) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(path));
 
-			List<List<Point2D_F64>> sets = new ArrayList<>();
-			String line = skipComments(reader);
-			if( line.compareTo("SETS") == 0 )
-				line = reader.readLine();
-			else {
-				// not a point sets file, assume everything is one set
-				sets.add( load(path));
-				return sets;
-			}
+            List<List<Point2D_F64>> sets = new ArrayList<>();
+            String line = skipComments(reader);
+            if (line.compareTo("SETS") == 0)
+                line = reader.readLine();
+            else {
+                // not a point sets file, assume everything is one set
+                sets.add(load(path));
+                return sets;
+            }
 
-			while( line != null ) {
-				List<Point2D_F64> set = new ArrayList<Point2D_F64>();
-				String words[] = line.split(" ");
+            while (line != null) {
+                List<Point2D_F64> set = new ArrayList<>();
+                String[] words = line.split(" ");
 
-				if( words.length%2 == 1 )
-					throw new RuntimeException("Odd number of words found on a line");
+                if (words.length % 2 == 1)
+                    throw new RuntimeException("Odd number of words found on a line");
 
-				for (int i = 0; i < words.length; i += 2 ) {
-					Point2D_F64 p = new Point2D_F64();
-					p.x = Double.parseDouble(words[i]);
-					p.y = Double.parseDouble(words[i+1]);
-					set.add(p);
-				}
-				sets.add(set);
-				line = reader.readLine();
-			}
+                for (int i = 0; i < words.length; i += 2) {
+                    Point2D_F64 p = new Point2D_F64();
+                    p.x = Double.parseDouble(words[i]);
+                    p.y = Double.parseDouble(words[i + 1]);
+                    set.add(p);
+                }
+                sets.add(set);
+                line = reader.readLine();
+            }
 
-			return sets;
+            return sets;
 
-		} catch (FileNotFoundException e) {
-			throw new RuntimeException(e);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
