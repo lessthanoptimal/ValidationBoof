d019e45946e06fb9b2b3745d69851e37786d0962
Peter Abeles <peter.abeles@gmail.com>
2021-12-06 16:32:36 -0800

    HandSelectEllipsesApp - Select file from dialog - Formatted code

--- src/main/java/boofcv/applications/HandSelectEllipsesApp.java
+++ src/main/java/boofcv/applications/HandSelectEllipsesApp.java
@@ -1,63 +1,61 @@
 package boofcv.applications;
 
 import boofcv.common.misc.EllipseFileCodec;
+import org.jetbrains.annotations.Nullable;
 
+import javax.swing.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 
 /**
- *
- *
  * @author Peter Abeles
  */
 public class HandSelectEllipsesApp extends HandSelectBase {
 
 
-	public HandSelectEllipsesApp( File file ) {
-		super(new SelectEllipsePanel(false),file);
-	}
+    public HandSelectEllipsesApp(@Nullable File file) {
+        super(new SelectEllipsePanel(false), file);
+    }
 
-	@Override
-	public void process(File file, BufferedImage image) {
-		this.infoPanel.setImageShape(image.getWidth(),image.getHeight());
-		SelectEllipsePanel gui = (SelectEllipsePanel)this.imagePanel;
+    @Override
+    public void process(File file, BufferedImage image) {
+        this.infoPanel.setImageShape(image.getWidth(), image.getHeight());
+        SelectEllipsePanel gui = (SelectEllipsePanel) this.imagePanel;
 
-		File outputFile = selectOutputFile(file);
-		if( outputFile.exists() ) {
-			gui.list.addAll(EllipseFileCodec.load(outputFile.getPath()));
-		}
+        File outputFile = selectOutputFile(file);
+        if (outputFile.exists()) {
+            gui.list.addAll(EllipseFileCodec.load(outputFile.getPath()));
+        }
 
-		gui.setImage(image);
-	}
+        gui.setImage(image);
+    }
 
-	@Override
-	public void save() {
-		SelectEllipsePanel gui = (SelectEllipsePanel)this.imagePanel;
-		File outputFile = selectOutputFile(inputFile);
-		EllipseFileCodec.save(outputFile.getPath()," rotated ellipses. x y a b phi",gui.list);
-	}
+    @Override
+    public void save() {
+        SelectEllipsePanel gui = (SelectEllipsePanel) this.imagePanel;
+        File outputFile = selectOutputFile(inputFile);
+        EllipseFileCodec.save(outputFile.getPath(), " rotated ellipses. x y a b phi", gui.list);
+    }
 
-	@Override
-	public String getApplicationName() {
-		return "Select Ellipses";
-	}
+    @Override
+    public String getApplicationName() {
+        return "Select Ellipses";
+    }
 
-	@Override
-	public void setScale( double scale ) {
-		SelectEllipsePanel gui = (SelectEllipsePanel)this.imagePanel;
-		gui.setScale(scale);
-	}
+    @Override
+    public void setScale(double scale) {
+        SelectEllipsePanel gui = (SelectEllipsePanel) this.imagePanel;
+        gui.setScale(scale);
+    }
 
-	@Override
-	public void clearPoints() {
-		SelectEllipsePanel gui = (SelectEllipsePanel)this.imagePanel;
-		gui.clearPoints();
-		gui.repaint();
-	}
+    @Override
+    public void clearPoints() {
+        SelectEllipsePanel gui = (SelectEllipsePanel) this.imagePanel;
+        gui.clearPoints();
+        gui.repaint();
+    }
 
-	public static void main(String[] args) {
-		String imagePath = "data/fiducials/circle_asymmetric/standard/distance_angle/image00000.jpg";
-
-		new HandSelectEllipsesApp(new File(imagePath));
-	}
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new HandSelectEllipsesApp(null));
+    }
 }
