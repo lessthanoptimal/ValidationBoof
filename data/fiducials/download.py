e658bda1b0c981a9cd611b4b4b5a0365b4fbee93
Peter Abeles <peter.abeles@gmail.com>
2022-08-08 08:04:25 -0700

    Updated download script

--- data/fiducials/download.py
+++ data/fiducials/download.py
@@ -4,7 +4,7 @@ import urllib.request
 from subprocess import call
 
 files = ["chessboard_v2.zip","circle_hexagonal_v1_1.zip","circle_regular_v1.zip",
-         "square_border_binary_v1.zip","square_border_image_v1.zip","square_grid_v1.zip","qrcodes_v3.zip",
+         "square_border_binary_v1.zip","square_border_image_v1.zip","square_grid_v1.zip","qrcodes_v4.zip",
          "random_dots_v1.zip", "square_border_hamming_v1.zip", "microqr_v1.zip", "aztec_codes_v1.zip"]
 
 for f in files:
