21b0b7e431892efa73269d74ad41d9cbffaf1bf2
Peter Abeles <peter.abeles@gmail.com>
2022-08-26 19:20:25 -0700

    - Added errors for when it views the same surface - Common visualization code

--- modules/geo/src/main/java/boofcv/metrics/mvs/DisparitySequenceConsistency.java
+++ modules/geo/src/main/java/boofcv/metrics/mvs/DisparitySequenceConsistency.java
@@ -4,13 +4,18 @@ import boofcv.abst.disparity.StereoDisparity;
 import boofcv.abst.geo.bundle.SceneObservations;
 import boofcv.abst.geo.bundle.SceneStructureMetric;
 import boofcv.alg.distort.ImageDistort;
+import boofcv.alg.distort.brown.LensDistortionBrown;
 import boofcv.alg.geo.PerspectiveOps;
 import boofcv.alg.geo.RectifyDistortImageOps;
+import boofcv.alg.geo.bundle.BundleAdjustmentOps;
 import boofcv.alg.geo.rectify.DisparityParameters;
+import boofcv.alg.misc.GImageMiscOps;
 import boofcv.alg.mvs.BundleToRectificationStereoParameters;
 import boofcv.core.image.GeneralizedImageOps;
 import boofcv.factory.disparity.ConfigDisparityBMBest5;
 import boofcv.factory.disparity.FactoryStereoDisparity;
+import boofcv.gui.ListDisplayPanel;
+import boofcv.gui.PointCloudViewerPanel;
 import boofcv.gui.image.ShowImages;
 import boofcv.gui.image.VisualizeImageData;
 import boofcv.io.UtilIO;
@@ -20,14 +25,22 @@ import boofcv.io.image.UtilImageIO;
 import boofcv.io.wrapper.DefaultMediaManager;
 import boofcv.misc.BoofMiscOps;
 import boofcv.struct.border.BorderType;
+import boofcv.struct.calib.CameraPinholeBrown;
 import boofcv.struct.image.*;
 import georegression.geometry.GeometryMath_F64;
 import georegression.struct.point.Point2D_F64;
+import georegression.struct.point.Point3D_F64;
 import georegression.struct.point.Point4D_F64;
 import georegression.struct.se.Se3_F64;
+import georegression.transform.se.SePointOps_F64;
+import org.ddogleg.stats.StatisticsDogArray;
+import org.ddogleg.struct.DogArray;
 import org.ddogleg.struct.DogArray_F32;
+import org.ddogleg.struct.DogArray_F64;
 import org.ddogleg.struct.DogArray_I32;
 
+import javax.swing.*;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -67,6 +80,15 @@ public class DisparitySequenceConsistency<Image extends ImageGray<Image>> {
     Class<Image> imageType;
 
     DogArray_F32 errors = new DogArray_F32();
+    DogArray<PixelError> perrors = new DogArray<>(PixelError::new);
+
+    // How long it took to process each log in milliseconds
+    DogArray_F64 timingMS = new DogArray_F64();
+
+    // Storage for
+    DogArray_F64 overallErrors95 = new DogArray_F64();
+
+    boolean showVisuals = true;
 
     public DisparitySequenceConsistency(StereoDisparity<Image, GrayF32> stereoDisparity) {
         this.stereoDisparity = stereoDisparity;
@@ -80,6 +102,7 @@ public class DisparitySequenceConsistency<Image extends ImageGray<Image>> {
 
     public boolean process(File directory) {
         this.directory = directory;
+        this.timingMS.reset();
 
         // Load configurations
         scene = MultiViewIO.load(new File(directory, "structure.yaml").getPath(), (SceneStructureMetric) null);
@@ -92,18 +115,53 @@ public class DisparitySequenceConsistency<Image extends ImageGray<Image>> {
         GrayF32 inverseDepth1 = new GrayF32(1, 1);
         GrayF32 inverseDepth2 = new GrayF32(1, 1);
 
-        for (int frameIdx = 7; frameIdx < frames.size - 3; frameIdx++) {
-            System.out.println("----------------- " + frameIdx);
+        // contains visuals when the key frame is the same
+        var panelSame = new ListDisplayPanel();
+        // Contains visuals when frames are different be viewing the same region
+        var panelDiff = new ListDisplayPanel();
 
-            // Compare offset by one
-            computeInverseDepth(frameIdx, frameIdx + 2, inverseDepth1);
-            computeInverseDepth(frameIdx, frameIdx + 3, inverseDepth2);
+        boolean first = true;
+
+        for (int frameIdx = 12; frameIdx < frames.size - 3; frameIdx++) {
+            // Same key frame but different 'other' frames
+            // This will catch biases and other artifacts when creating a single disparity image
+            computeInverseDepth(frameIdx, frameIdx + 1, inverseDepth1);
+            computeInverseDepth(frameIdx, frameIdx + 2, inverseDepth2);
+            out.printf("Frame: %3d same   ", frameIdx);
             errorMetricsSameFrame(inverseDepth1, inverseDepth2);
-            BufferedImage vis = visualizeError(inverseDepth1, inverseDepth2);
-            ShowImages.showWindow(vis, "Error "+frameIdx, true);
 
-            // Compare when you flip left and right images
+            if (first && showVisuals) {
+                first = false;
+                SwingUtilities.invokeLater(() -> {
+                    panelSame.setPreferredSize(new Dimension(inverseDepth1.width, inverseDepth1.height));
+                    panelDiff.setPreferredSize(panelSame.getPreferredSize());
+                    ShowImages.showWindow(panelSame, "Same Error", true);
+                    ShowImages.showWindow(panelDiff, "Mutual Error", true);
+                });
+            }
+
+            String frameName = "" + frameIdx;
+
+            if (showVisuals)
+                SwingUtilities.invokeLater(() -> panelSame.addImage(visualizeError(inverseDepth1.width, inverseDepth1.height), frameName));
+
+            // Two key frames that observe overlapping regions
+            // If there's a persistent artifact when the left frame is the same it will show up here.
+            // This was inpired by the surface curving
+            computeInverseDepth(frameIdx + 1, frameIdx + 3, inverseDepth1);
+            out.printf("Frame: %3d mutual ", frameIdx);
+            errorMetricsReprojected(frameIdx, inverseDepth2, frameIdx + 1, inverseDepth1);
+//            errorMetricsMutualProj(frameIdx + 1, inverseDepth1, frameIdx, inverseDepth2);
+
+            if (showVisuals)
+                SwingUtilities.invokeLater(() -> panelDiff.addImage(visualizeError(inverseDepth1.width, inverseDepth1.height), frameName));
+
+//            displayCloud(frameIdx, inverseDepth2);
         }
+
+        out.println();
+        double average = StatisticsDogArray.mean(overallErrors95);
+        out.printf("Overall: N=%d average_P95=%.4f\n", overallErrors95.size, average);
 
         return true;
     }
@@ -113,6 +171,7 @@ public class DisparitySequenceConsistency<Image extends ImageGray<Image>> {
      */
     private void errorMetricsSameFrame(GrayF32 inverse1, GrayF32 inverse2) {
         errors.reset();
+        perrors.reset();
         for (int y = 0; y < inverse1.height; y++) {
             for (int x = 0; x < inverse1.width; x++) {
                 float v1 = inverse1.unsafe_get(x, y);
@@ -122,10 +181,16 @@ public class DisparitySequenceConsistency<Image extends ImageGray<Image>> {
                 if (Double.isNaN(v1) || Double.isNaN(v2) || v1 == 0.0 || v2 == 0.0) {
                     continue;
                 }
-                errors.add((1.0f/v2) - (1.0f/v1));
+                float error = (1.0f / v2) - (1.0f / v1);
+                errors.add(error);
+                perrors.grow().setTo(x, y, error);
             }
         }
-        double overlap = 100.0*((double)errors.size/(inverse1.width*inverse1.height));
+        printMetrics(inverse1);
+    }
+
+    private void printMetrics(GrayF32 inverse1) {
+        double overlap = 100.0 * ((double) errors.size / (inverse1.width * inverse1.height));
 
         float mean = 0.0f;
         for (int i = 0; i < errors.size; i++) {
@@ -135,51 +200,128 @@ public class DisparitySequenceConsistency<Image extends ImageGray<Image>> {
         mean /= errors.size;
 
         errors.sort();
-        System.out.printf("%3d%% mean=%6.3f p50=%.3f p95=%.3f max=%.3f\n",
-                (int)overlap, mean, errors.getFraction(0.5), errors.getFraction(0.95), errors.getFraction(1.0));
+        out.printf("overlap: %3d%% mean=%6.3f p50=%.3f p95=%.3f p995=%.3f max=%.3f\n",
+                (int) overlap, mean,
+                errors.getFraction(0.50), errors.getFraction(0.95),
+                errors.getFraction(0.995), errors.getFraction(1.00));
+
+        overallErrors95.add(errors.getFraction(0.95));
     }
 
-    private void errorMetricsMutualProj(int idx1, GrayF32 inverse1, int idx2, GrayF32 inverse2) {
+    private void errorMetricsReprojected(int idx1, GrayF32 inverse1, int idx2, GrayF32 inverse2) {
+        var intrinsic = new CameraPinholeBrown();
+        BundleAdjustmentOps.convert(scene.getViewCamera(idx1).model, 0, 0, intrinsic);
+        var pixelToNorm = new LensDistortionBrown(intrinsic).undistort_F64(true, false);
+        var normToPixel = new LensDistortionBrown(intrinsic).distort_F64(false, true);
+
+        Se3_F64 a_to_b = scene.getViewToView(idx1, idx2);
+
+        var norm = new Point2D_F64();
+        var point1 = new Point4D_F64();
+        var point2 = new Point4D_F64();
+        var pixel2 = new Point2D_F64();
+
         errors.reset();
         for (int y = 0; y < inverse1.height; y++) {
             for (int x = 0; x < inverse1.width; x++) {
                 float v1 = inverse1.unsafe_get(x, y);
-                if (Double.isNaN(v1)) {
+                if (v1 == 0.0 || Float.isNaN(v1)) {
                     continue;
                 }
+
+                // Get point in view-1's reference frame
+                pixelToNorm.compute(x, y, norm);
+                point1.setTo(norm.x, norm.y, 1.0, v1);
+
+                // Put it into view-2's reference frame
+                SePointOps_F64.transform(a_to_b, point1, point2);
+
+                if (PerspectiveOps.isBehindCamera(point2))
+                    continue;
+
+                // See which pixel it's on
+                normToPixel.compute(point2.x / point2.z, point2.y / point2.z, pixel2);
+
+                int x2 = (int) (pixel2.x + 0.5);
+                int y2 = (int) (pixel2.y + 0.5);
+                if (!inverse2.isInBounds(x2, y2))
+                    continue;
+
+                float v2 = inverse1.unsafe_get(x2, y2);
+                if (v2 == 0.0f || Float.isNaN(v2))
+                    continue;
+
+                float error = (float) (point2.z / point2.w) - (1.0f / v2);
+                errors.add(error);
+                perrors.grow().setTo(x, y, error);
             }
         }
+
+        printMetrics(inverse1);
     }
 
-    private BufferedImage visualizeError(GrayF32 inverse1, GrayF32 inverse2) {
-        GrayF32 error = inverse1.createSameShape();
+    private BufferedImage visualizeError(int width, int height) {
+        GrayF32 errorImage = new GrayF32(width, height);
 
         float maxAllowedError = 2.0f;
-        for (int y = 0; y < inverse1.height; y++) {
-            for (int x = 0; x < inverse1.width; x++) {
-                float v1 = inverse1.unsafe_get(x, y);
-                float v2 = inverse2.unsafe_get(x, y);
 
-                if (Double.isNaN(v1) || Double.isNaN(v2) || v1 == 0.0 || v2 == 0.0) {
-                    error.unsafe_set(x, y, Float.NaN);
+        GImageMiscOps.fill(errorImage, Float.NaN);
+        perrors.forEach(e -> {
+            // Ensure the error scale is within an allowed value for visualization
+            float error = e.error;
+            if (Math.abs(error) > maxAllowedError) {
+                error *= maxAllowedError / Math.abs(error);
+            }
+            errorImage.unsafe_set(e.x, e.y, error);
+        });
+
+        return VisualizeImageData.colorizeSign(errorImage, null, maxAllowedError);
+    }
+
+    private void displayCloud(int imageIdx, GrayF32 inverseDepth) {
+        BufferedImage image = loadBufferedImage(imageIdx, directory);
+        var intrinsic = new CameraPinholeBrown();
+        BundleAdjustmentOps.convert(scene.getViewCamera(imageIdx).model,
+                image.getWidth(), image.getHeight(), intrinsic);
+
+        var pixelToNorm = new LensDistortionBrown(intrinsic).undistort_F64(true, false);
+        var norm = new Point2D_F64();
+
+        var cloud = new DogArray<>(Point3D_F64::new);
+        var colors = new DogArray_I32();
+
+        for (int y = 0; y < inverseDepth.height; y++) {
+            for (int x = 0; x < inverseDepth.width; x++) {
+                float v = inverseDepth.unsafe_get(x, y);
+                // Skip over points at infinity or very far away
+                if (Float.isNaN(v) || v <= 1e-8) {
                     continue;
                 }
-                float diff = (1.0f/v2) - (1.0f/v1);
 
-                // Ensure the error scale is within an allowed value for visualization
-                if (Math.abs(diff) > maxAllowedError) {
-                    diff *= maxAllowedError/Math.abs(diff);
-                }
-                error.unsafe_set(x, y, diff);
+                pixelToNorm.compute(x, y, norm);
+
+                cloud.grow().setTo(norm.x / v, norm.y / v, 1.0 / v);
+                colors.add(image.getRGB(x, y));
             }
         }
 
-        return VisualizeImageData.colorizeSign(error, null, maxAllowedError);
+        SwingUtilities.invokeLater(() -> {
+            var viewer = new PointCloudViewerPanel();
+            viewer.addCloud64(cloud.toList(), colors);
+            viewer.getViewer().setCameraHFov(PerspectiveOps.computeVFov(intrinsic));
+            viewer.getViewer().setTranslationStep(0.2);
+            viewer.setPreferredSize(new Dimension(intrinsic.width / 2, intrinsic.height / 2));
+
+            ShowImages.showWindow(viewer, "Cloud " + imageIdx);
+        });
+        BoofMiscOps.sleep(60_000);
     }
 
     private void computeInverseDepth(int idx1, int idx2, GrayF32 inverseDepth) {
         Image imgA = loadImage(idx1, directory);
         Image imgB = loadImage(idx2, directory);
+
+        long time0 = System.currentTimeMillis();
 
         Se3_F64 a_to_b = scene.getViewToView(idx1, idx2);
 
@@ -207,6 +349,8 @@ public class DisparitySequenceConsistency<Image extends ImageGray<Image>> {
         PerspectiveOps.matrixToPinhole(rectParam.rectifiedK, rectifiedShape.width, rectifiedShape.height, dispParam.pinhole);
 
         transferDepthToOriginal(stereoDisparity.getDisparity(), inverseDepth);
+
+        timingMS.add(System.currentTimeMillis() - time0);
     }
 
     /**
@@ -310,10 +454,29 @@ public class DisparitySequenceConsistency<Image extends ImageGray<Image>> {
         return Objects.requireNonNull(image, "Image not found");
     }
 
+    private BufferedImage loadBufferedImage(int frameIdx, File directory) {
+        String name = String.format("images/frame%04d.png", frameIdx);
+        return UtilImageIO.loadImage(new File(directory, name).getPath());
+    }
+
+    static class PixelError {
+        int x;
+        int y;
+        float error;
+
+        public PixelError setTo(int x, int y, float error) {
+            this.x = x;
+            this.y = y;
+            this.error = error;
+
+            return this;
+        }
+    }
+
     public static void main(String[] args) {
         var config = new ConfigDisparityBMBest5();
-        config.disparityRange = 255;
-        config.regionRadiusX = config.regionRadiusY = 6;
+        config.disparityRange = 250;
+        config.regionRadiusX = config.regionRadiusY = 12;
 
         StereoDisparity<GrayF32, GrayF32> stereoDisparity = FactoryStereoDisparity.blockMatchBest5(config, GrayF32.class, GrayF32.class);
 
@@ -321,6 +484,5 @@ public class DisparitySequenceConsistency<Image extends ImageGray<Image>> {
 
         var app = new DisparitySequenceConsistency<>(stereoDisparity);
         app.process(new File(path));
-        System.out.println("Done!");
     }
 }
